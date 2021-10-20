5fee1aa398fe5a4079310dde6d7bf538a3d6c4a0
rodrigo <prof-rodrigo-silva@users.noreply.github.com>
2021-10-20 16:04:38 -0300

    Segundo commit

--- projetoteste1/src/projetoteste1/Aluno.java
+++ projetoteste1/src/projetoteste1/Aluno.java
@@ -2,6 +2,41 @@ package projetoteste1;
 
 public class Aluno {
 	
+	
+	public Aluno() {
+		
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + idade;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (idade != other.idade)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", idade=" + idade + "]";
+	}
 	private String nome;
 	private int idade;
 	public String getNome() {
